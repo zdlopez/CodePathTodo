2ded1cd0838a047be12ef9ba34991888de9b4ad7
Zachary Lopez <zachary.d.lopez@gmail.com>
2016-06-26 13:09:50 -0700

    adds basic app funcitonality, also click listener for edit event

---
+++ app/src/main/java/com/zdlopez/codepathtodo/MainActivity.java
@@ -0,0 +1,104 @@
+package com.zdlopez.codepathtodo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<>();
+        readItems();
+        itemsAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        setupListViewListener();
+        lvItems.setOnItemClickListener(new ListClickHandler());
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+
+    public void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        items.remove(pos);
+                        itemsAdapter.notifyDataSetChanged();
+                        writeItems();
+                        return true;
+                    }
+                });
+    }
+
+    public class ListClickHandler implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> adapter, View view, int pos, long id) {
+            // TODO Auto-generated method stub
+//            TextView listText = (TextView) view.findViewById(R.id.listText);
+//            String text = listText.getText().toString();
+
+            // create intent to start another activity
+            //Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            // add the selected text item to our intent.
+            //intent.putExtra("selected-item", text);
+            //startActivity(intent);
+            items.remove(pos);
+            itemsAdapter.notifyDataSetChanged();
+            writeItems();
+        }
+
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
